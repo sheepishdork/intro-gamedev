a1453e5eac6564f70de3b4b5157f5f7c0ceec5e8
sheepishdork <35929680+sheepishdork@users.noreply.github.com>
2018-02-15 17:25:44 -0500

    Add files via upload

---
+++ DialogueManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+
+    // Use this for initialization
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Starting conversation with" + dialogue.name);
+        }
+    }
+}
+	
